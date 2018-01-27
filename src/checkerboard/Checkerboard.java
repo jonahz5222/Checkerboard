f5e2f553d6bb6fbaabd0a601415dcdb2f24e805f
Jonah Zukosky <jonah.zukosky@gmail.com>
2018-01-27 14:26:07 -0600

    Created Constructors for Checkerboard class

--- src/checkerboard/Checkerboard.java
+++ src/checkerboard/Checkerboard.java
@@ -9,6 +9,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 /**
@@ -16,6 +17,31 @@ import javafx.stage.Stage;
  * @author jonahzukosky
  */
 public class Checkerboard extends Application {
+    
+    private int numRows;
+    private int numColumns;
+    
+    private double boardWidth;
+    private double boardHeight;
+    
+    private final Color DEFAULT_DARK = Color.BLACK;
+    private final Color DEFAULT_LIGHT = Color.RED;
+    
+    private Color darkColor;
+    private Color lightColor;
+    
+    public void Checkerboard(int numRows,int numColumns,double boardWidth, double boardHeight){
+        this.numRows = numRows;
+        this.numColumns = numColumns;
+        this.boardWidth = boardWidth;
+        this.boardHeight = boardHeight;
+    }
+    public void Checkerboard(int numRows,int numColumns,double boardWidth,double boardHeight,Color darkColor,Color lightColor){
+        this.darkColor = darkColor;
+        this.lightColor = lightColor;
+        
+        Checkerboard(numRows,numColumns,boardWidth,boardHeight);
+    }
     
     @Override
     public void start(Stage stage) throws Exception {
