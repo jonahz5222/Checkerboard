8a56a1852bc3ef428f131fa5d5bcef63d49f97ef
Jonah Zukosky <jonah.zukosky@gmail.com>
2018-01-30 11:40:42 -0600

    Catch-up/forgot to commit yesterday

--- src/checkerboard/CheckerboardFXMLController.java
+++ src/checkerboard/CheckerboardFXMLController.java
@@ -68,7 +68,7 @@ public class CheckerboardFXMLController implements Initializable,Startable {
         this.stage = stage;
         
         ChangeListener<Number> lambdaChangeListener = ((ObservableValue<? extends Number> observable, Number oldValue, final Number newValue) -> {
-        newBoard(new CheckerboardClass(this.checkerBoard.getNumRows(), this.checkerBoard.getNumColumns(), stage.getScene().getWidth(),stage.getScene().getHeight() - menuBar.getHeight(), this.checkerBoard.getLightColor(),this.checkerBoard.getDarkColor()));
+            newBoard(new CheckerboardClass(this.checkerBoard.getNumRows(), this.checkerBoard.getNumColumns(), stage.getScene().getWidth(),stage.getScene().getHeight() - menuBar.getHeight(), this.checkerBoard.getLightColor(),this.checkerBoard.getDarkColor()));
         });
         
         newBoard(new CheckerboardClass(8,8,anchorPane.getWidth(),anchorPane.getHeight(),Color.RED,Color.BLACK));
